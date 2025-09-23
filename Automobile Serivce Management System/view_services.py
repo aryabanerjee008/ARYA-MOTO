a1a58ff621adad5fc2b250954099484e9e63a0e1
Archit Aryan <architaryan20@gmail.com>
2025-09-24 00:21:15 +0530

    update view_services.py

--- Automobile Serivce Management System/view_services.py
+++ Automobile Serivce Management System/view_services.py
@@ -4,3 +4,17 @@ import pyfiglet
 def main():
     text = pyfiglet.print_figlet(text = "Service  View", colors = "YELLOW", width = 100)
     print("\n" * 2)
+
+#user_auth
+
+print(" Security Authentication")
+print(" Please verify your identity")
+vri_id = input(char(" Enter user ID:")
+vri_pss = input(char(" Enter Password:")
+
+def check():
+    if vri_id == user_id:
+    ver_pss == user_pss
+    break
+elif:
+    print(" Please enter Correct Credentials")
